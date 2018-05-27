e9d465f9f940a481600c3a247cc575feefc8b90a
Yaniv Silberman <yanivsilberman@Yanivs-MacBook-Pro.local>
2018-05-27 18:32:45 +0200

    trying different babel production scripts

--- build/starter.js
+++ build/starter.js
@@ -1,8 +1,11 @@
+'use strict';
+
 require('babel-register')({
-    presets: [ 'env' ]
+    presets: ['env']
 });
 // require("babel-core/register");
 // require("babel-polyfill");
 
 // Import the rest of our application.
 module.exports = require('../bin/www');
+//# sourceMappingURL=starter.js.map
