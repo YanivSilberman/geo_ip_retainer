43686c7282f2b30b501ff7c7ca26ddaee125230f
Yaniv Silberman <yanivsilberman@Yanivs-MacBook-Pro.local>
2018-05-29 13:10:06 +0200

    fixed extracting jsonp

--- library/index.js
+++ library/index.js
@@ -4,11 +4,25 @@ import moment from 'moment';
 const ipstack = ip =>
   `http://api.ipstack.com/${ip}?access_key=${process.env.API_STACK_KEY}&callback=json`;
 
+const extractJson = jsonp => {
+  let json;
+  try {
+    json = JSON.parse(jsonp)
+  } catch(err) {
+    let startPos = jsonp.indexOf('({');
+    let endPos = jsonp.indexOf('})');
+    let jsonString = jsonp.substring(startPos+1, endPos+1);
+    json = JSON.parse(jsonString);
+  }
+
+  return json;
+}
+
 export const makeIpStackReq = (ipAddress, callback) =>
   request(ipstack(ipAddress), (error, response, body) => {
-    // console.log('headers', response.headers);
+    const json = extractJson(body);
     if (!error && response.statusCode == 200) {
-      callback(body);
+      callback(json);
     }
 });
 
